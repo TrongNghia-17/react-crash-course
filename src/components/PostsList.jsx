aae007b7983d31ea61bbea12f9836952cf329bf1
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-08-08 22:06:04 +0700

    video 30

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,14 +4,9 @@ import NewPost from "./NewPost";
 import Classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
-function PostList() {
-    const [modalIsVisible, setModalIsVisible] = useState(true);
+function PostList({ isPosting, onStopPosting }) {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function hideModalHandler() {
-        setModalIsVisible(false);
-    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -22,7 +17,7 @@ function PostList() {
 
     return (
         <>
-            {modalIsVisible ? <Modal onClose={hideModalHandler}>
+            {isPosting ? <Modal onClose={onStopPosting}>
                 <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
             </Modal> : false}
 
