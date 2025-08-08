d45550b97fe9647d035c35bd914b8dd273da4ac6
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-08-08 09:17:18 +0700

    video 19

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,14 @@
+const names = ['Nghia', 'Han'];
+
 function Post() {
-    return <div>
-        <p>Huynh Trong Nghia</p>
-        <p>React</p>
-    </div>
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+    return (
+        <div>
+            <p>{chosenName}</p>
+            <p>React</p>
+        </div>
+    );
 }
 
 export default Post;
