153da84794c34d1398e123c8ea845fafab8e4e05
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-08-09 11:00:26 +0700

    video 37

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Classes from "./PostsList.module.css";
@@ -6,6 +6,16 @@ import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+
+        fetchPosts();
+    }, []);
 
     function addPostHandler(postData) {
         fetch('http://localhost:8080/posts', {
