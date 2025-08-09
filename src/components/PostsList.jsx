e63b3755656bb6e27ec3413d96759cb2b61e4181
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-08-09 20:15:19 +0700

    video 42

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
 import Post from "./Post";
-import NewPost from "./NewPost";
 import Classes from "./PostsList.module.css";
-import Modal from "./Modal";
 
-function PostList({ isPosting, onStopPosting }) {
+function PostList() {
     const [posts, setPosts] = useState([]);
     const [isFetching, setIsFetching] = useState(false);
 
@@ -33,12 +31,6 @@ function PostList({ isPosting, onStopPosting }) {
 
     return (
         <>
-            {isPosting ? <Modal onClose={onStopPosting}>
-                <NewPost
-                    onCancel={onStopPosting}
-                    onAddPost={addPostHandler}
-                />
-            </Modal> : false}
             {!isFetching && posts.length > 0 &&
                 (<ul className={Classes.posts}>
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App'
+import Posts from './routers/Posts'
 import './index.css'
-import NewPost from './components/NewPost'
+import NewPost from './routers/NewPost'
 import RootLayout from './routers/RootLayout'
 
 const router = createBrowserRouter([
   {
-    path: '/', element: < RootLayout />, children: [
-      { path: '/', element: <App /> },
-      { path: '/create-post', element: <NewPost /> }
+    path: '/',
+    element: < RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        children: [{ path: '/create-post', element: <NewPost /> }]
+      },
     ]
   },
 ]);
